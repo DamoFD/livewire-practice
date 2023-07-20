e25730f0ee14d5c56ddef4b3c42c62e56112a6d6
DamoFD <voshall@yahoo.com>
2023-07-19 17:28:31 -0700

    first commit

---
+++ app/Http/Livewire/Comments.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Carbon\Carbon;
+use Livewire\Component;
+
+class Comments extends Component
+{
+
+    public $comments = [
+        [
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci commodi eligendi, illum nemo placeat sequi! Adipisci labore molestiae odit. Excepturi?',
+            'created_at' => '3 min ago',
+            'creator' => 'Sarthak',
+        ],
+    ];
+
+    public $newComment;
+
+    public function addComment()
+    {
+        array_unshift($this->comments, [
+            'body' => $this->newComment,
+            'created_at' => Carbon::now()->diffForHumans(),
+            'creator' => 'Sarthak',
+        ]);
+        $this->newComment = '';
+
+    }
+
+    public function render()
+    {
+        return view('livewire.comments');
+    }
+}
