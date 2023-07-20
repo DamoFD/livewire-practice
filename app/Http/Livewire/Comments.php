8929f89b3bc07d5a3184689b8ecdfdcbc3c4db99
DamoFD <voshall@yahoo.com>
2023-07-19 21:11:07 -0700

    realtime pagination

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -5,18 +5,13 @@ namespace App\Http\Livewire;
 use App\Models\Comment;
 use Carbon\Carbon;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Comments extends Component
 {
-
-    public $comments;
+    use WithPagination;
 
     public $newComment;
-
-    public function mount()
-    {
-        $this->comments = Comment::latest()->get();
-    }
 
     public function updated($field)
     {
@@ -34,8 +29,6 @@ class Comments extends Component
             'user_id' => 1,
         ]);
 
-        $this->comments->prepend($createdComment);
-
         $this->newComment = '';
 
         session()->flash('message', 'Comment added successfully');
@@ -45,13 +38,14 @@ class Comments extends Component
     {
         $comment = Comment::find($commentId);
         $comment->delete();
-        $this->comments = $this->comments->except($commentId);
 
         session()->flash('message', 'Comment deleted successfully');
     }
 
     public function render()
     {
-        return view('livewire.comments');
+        return view('livewire.comments', [
+            'comments' => Comment::latest()->paginate(2),
+        ]);
     }
 }
